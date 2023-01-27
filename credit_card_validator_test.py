34eb77dd6d93aee26f2e2029c167d4090ed1babb
reniike <112325698+reniike@users.noreply.github.com>
2023-01-27 14:49:16 +0100

    Update credit_card_validator_test.py

--- credit_card_validator_test.py
+++ credit_card_validator_test.py
@@ -6,7 +6,7 @@ from credit_card_validator import card_confirmation
 class TestCreditCardValidator(unittest.TestCase):
 
     def test_valid_credit_card(self):
-        self.assertEqual(card_confirmation("5399834432138592"), "Valid credit card")
+        self.assertEqual(card_confirmation("5399834432138592"), "Card is Valid")
 
     def test_invalid_credit_card(self):
-        self.assertEqual(card_confirmation("539983443213859"), "Valid credit card")
+        self.assertEqual(card_confirmation("539983443213859"), "Card is Invalid")
